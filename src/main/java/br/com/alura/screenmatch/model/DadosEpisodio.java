93415f3b24566d123dd845c2fc23ed1bd0c847e3
Marcus Salviano <marcussalviano@gmail.com>
2024-06-10 11:53:25 -0400

    Updating aplication models.

--- src/main/java/br/com/alura/screenmatch/model/DadosEpisodio.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosEpisodio.java
@@ -1,4 +1,4 @@
-package br.com.alura.screenmatch.service;
+package br.com.alura.screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
